f9bcb64ba10e0504cfd2d889e1e6cf125a9c9ac5
Sargis Sargsyan <sargis.sargsyan@live.com>
2025-10-11 12:05:20 +0400

    create project Test

--- src/main/java/org/example/CreateProject.java
+++ src/main/java/org/example/CreateProject.java
@@ -1,8 +1,74 @@
 package org.example;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.example.models.Project;
+import org.example.models.ProjectRequestBody;
+import org.example.models.RegisterRequestBody;
+import org.example.models.User;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import java.io.IOException;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
 
 public class CreateProject {
-    public static void main(String[] args) throws IOException {
+    private User createdUser;
+    private RequestSpecification requestSpecification;
+    private ResponseSpecification responseSpecification;
 
+    @BeforeMethod
+    public void beforeMethod() {
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("http://localhost").setPort(9000)
+                .setContentType(ContentType.JSON)
+                .build();
+        responseSpecification = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+        String username = "picartacademy" + new Random().nextInt(100);
+        String email = "picsartacademy"+ + new Random().nextInt(100) + "@gmail.com";
+        RegisterRequestBody requestBody = new RegisterRequestBody();
+        requestBody.setUsername(username);
+        requestBody.setPassword("Picsart12345");
+        requestBody.setAccepted_terms(true);
+        requestBody.setFull_name("Picsart Academy");
+        requestBody.setEmail(email);
+        requestBody.setType("public");
+        createdUser = given()
+                .spec(requestSpecification)
+                .body(requestBody).
+                when()
+                .post("/api/v1/auth/register").
+                then().log().all()
+                .spec(responseSpecification)
+                .statusCode(201)
+                .extract().as(User.class);
+    }
+
+    @Test
+    public void createProject() {
+        ProjectRequestBody project = new ProjectRequestBody();
+        project.setName("Test Project Name");
+        project.setDescription("Test Project Description");
+        project.setCreation_template(1);
+        project.setIs_private(false);
+
+        Project createdProject = given()
+                    .spec(requestSpecification)
+                    .body(project)
+                    .header("Authorization", "Bearer " + createdUser.getAuth_token()).
+                when()
+                    .post("/api/v1/projects")
+                .then().log().all()
+                    .spec(responseSpecification)
+                    .statusCode(201)
+                    .extract().as(Project.class);
+
+        System.out.println(createdProject);
     }
 }
---
+++ src/main/java/org/example/models/ProjectRequestBody.java
@@ -0,0 +1,41 @@
+package org.example.models;
+
+public class ProjectRequestBody {
+
+    private String name;
+    private String description;
+    private int creation_template;
+    private boolean is_private;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreation_template() {
+        return creation_template;
+    }
+
+    public void setCreation_template(int creation_template) {
+        this.creation_template = creation_template;
+    }
+
+    public boolean isIs_private() {
+        return is_private;
+    }
+
+    public void setIs_private(boolean is_private) {
+        this.is_private = is_private;
+    }
+}
