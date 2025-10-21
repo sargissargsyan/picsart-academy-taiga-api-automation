286cb4655f76f297e0358fca977180ca26a873c7
Sargis Sargsyan <sargis.sargsyan@live.com>
2025-10-21 10:32:59 +0400

    created BaseService

---
+++ src/main/java/io/taiga/api/services/BaseService.java
@@ -0,0 +1,50 @@
+package io.taiga.api.services;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.port;
+
+public class BaseService {
+    private static final String BASE_URL = "http://localhost";
+    private static final int BASE_URL_PORT = 9000;
+
+    protected static RequestSpecification configBaseRequest() {
+        return given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URL)
+                .port(BASE_URL_PORT);
+    }
+
+    protected static Map<String, String> setToken(String token) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + token);
+        return headers;
+    }
+
+    protected static Response get(RequestSpecification requestSpecification) {
+        requestSpecification.log().ifValidationFails();
+        return requestSpecification.get();
+    }
+
+    protected static Response post(RequestSpecification requestSpecification) {
+        requestSpecification.log().ifValidationFails();
+        return requestSpecification.post();
+    }
+
+    protected static Response patch(RequestSpecification requestSpecification) {
+        requestSpecification.log().ifValidationFails();
+        return requestSpecification.patch();
+    }
+
+    protected static Response delete(RequestSpecification requestSpecification) {
+        requestSpecification.log().ifValidationFails();
+        return requestSpecification.delete();
+    }
+
+}
