e2e2c779f41cf1e0bcee6a26eb20ea472aea8067
Sargis Sargsyan <sargis.sargsyan@live.com>
2025-11-08 11:48:15 +0400

    Refactor Selenium test with setup and teardown methods
    Replaced static main method with TestNG lifecycle annotations (`@BeforeClass`, `@BeforeMethod`, `@AfterMethod`). Added a sample test for interacting with a web form, improving test organization and readability.

--- src/test/java/io/taiga/selenium/FirstSeleniumTest.java
+++ src/test/java/io/taiga/selenium/FirstSeleniumTest.java
@@ -1,15 +1,51 @@
 package io.taiga.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class FirstSeleniumTest {
+    private WebDriver driver;
 
-    public static void main(String[] args) {
+    @BeforeClass
+    public void beforeClass() {
         WebDriverManager.chromedriver().setup();
-        WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://www.google.com/");
+    }
+
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    @Test
+    public void uiTest() {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        assertEquals(driver.getTitle(), "Web form", "Title was Incorrect!");
+
+        driver.findElement(By.id("my-text-id")).sendKeys("Hello Selenium!");
+        driver.findElement(By.cssSelector("#my-text-id"));
+        driver.findElement(By.cssSelector("[id='my-text-id']"));
+        driver.findElement(By.xpath("//*[@id=\"my-text-id\"]"));
+        driver.findElement(By.name("my-text"));
+        driver.findElement(By.cssSelector("[myprop='myvalue']"));
+
 
     }
 }
