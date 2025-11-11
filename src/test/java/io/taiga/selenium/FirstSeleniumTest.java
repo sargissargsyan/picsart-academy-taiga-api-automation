3f0367dbede5a50551d6ab6fe21396e5620e16ba
Sargis Sargsyan <sargis.sargsyan@live.com>
2025-11-11 10:47:59 +0400

    Add new Selenium tests for input values and relative locators
    Introduced new test cases to verify input field values, relative locators, and button interactions. Enhanced coverage by asserting element attributes and behaviors on the web form. These additions improve test robustness and ensure accuracy.

--- src/test/java/io/taiga/selenium/FirstSeleniumTest.java
+++ src/test/java/io/taiga/selenium/FirstSeleniumTest.java
@@ -3,7 +3,9 @@ package io.taiga.selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -45,7 +47,41 @@ public class FirstSeleniumTest {
         driver.findElement(By.xpath("//*[@id=\"my-text-id\"]"));
         driver.findElement(By.name("my-text"));
         driver.findElement(By.cssSelector("[myprop='myvalue']"));
+        driver.findElement(RelativeLocator.with(By.cssSelector("[myprop='myvalue']")));
+        driver.findElement(By.tagName("textarea")).sendKeys("Hello Selenium!");
+        driver.findElement(By.cssSelector("[name='my-disabled']"));
 
 
     }
+
+    @Test
+    public void valueTest() {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement readOnlyElement = driver.findElement(By.name("my-readonly"));
+        String readOnlyValue = readOnlyElement.getAttribute("value");
+
+        assertEquals(readOnlyValue, "Readonly input", "Value was Incorrect!");
+    }
+
+    @Test
+    public void inputValueTest() {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement textInput = driver.findElement(By.id("my-text-id"));
+        textInput.sendKeys("Hello Selenium!");
+        assertEquals(textInput.getAttribute("value"), "Hello Selenium!", "Value was Incorrect!");
+    }
+
+    @Test
+    public void testRelativeLocators() {
+        driver.get( "https://www.selenium.dev/selenium/web/web-form.html");
+        driver.findElement(By.id("my-text-id")).sendKeys("Hello Selenium!");
+        WebElement button = driver.findElement(By.cssSelector("button[type='submit']"));
+        button.click();
+
+        String message = driver.findElement(By.cssSelector(".lead")).getText();
+
+        assertEquals(message, "Received!", "Label was Incorrect!");
+    }
+
+
 }
